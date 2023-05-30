aae8b042d083952096e5b1fbfe02184fb98fb05b
Full Name <njdowdle@gmail.com>
2023-05-30 21:56:32 +0700

    added buttons and js

--- rockScript.js
+++ rockScript.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+var computer = function() {
     var computerChoice = Math.floor(Math.random() * 3);
     console.log(`computer choice number is ${computerChoice}`)
     if (computerChoice === 0) {
@@ -10,22 +10,28 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerChoice) {
+var handler = function(playerSelection) {
+    var computerChoice = computer();
     if (playerSelection === 'Paper' && computerChoice === 'Rock') {
+        console.log(`You win, ${playerSelection} beats ${computerChoice}`);
         return `You win, ${playerSelection} beats ${computerChoice}`;
     } else if (playerSelection === 'Rock' && computerChoice === 'Scissors') {
+        console.log(`You win, ${playerSelection} beats ${computerChoice}`);
         return `You win, ${playerSelection} beats ${computerChoice}`;
     } else if (playerSelection === 'Scissors' && computerChoice === 'Paper') {
+        console.log(`You win, ${playerSelection} beats ${computerChoice}`);
         return `You win, ${playerSelection} beats ${computerChoice}`;
     } else if (playerSelection === computerChoice) {
+        console.log(`You rolled ${playerSelection} and opponent rolled ${computerChoice}. Tie!`);
         return `You rolled ${playerSelection} and opponent rolled ${computerChoice}. Tie!`
     } else {
+        console.log(`You lose, ${computerChoice} beats ${playerSelection}`);
         return `You lose, ${computerChoice} beats ${playerSelection}`;
     }
 }
 
 function game() {
-    var score = 0;
+    /*var score = 0;
     roundsPlayed = 5;
     for (let i = 0; i < roundsPlayed; i++) {
         var playerSelection = prompt("Choose \'Rock\', \'Paper\', or \'Scissors\'");
@@ -33,7 +39,25 @@ function game() {
         console.log(playRound(playerSelection, computerSelection));
     }
 
-    return score;
+    return score;*/
 }
 
-game();
+function setUp() {
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
+
+    rock.addEventListener('click', function() {
+        handler('Rock');
+    });
+    paper.addEventListener('click', function() {
+        handler('Paper');
+    });
+    scissors.addEventListener('click', function() {
+        handler('Scissors');
+    });
+}
+
+setUp();
+
+//game();
